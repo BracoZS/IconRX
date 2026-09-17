48bf24ca7d174f04df452d7d06c5382f6c669264
BracoZS <BracoZS@users.noreply.github.com>
2026-09-16 21:03:06 -0500

    initial commit

---
+++ IconRX/IconRXDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IconRXDemo;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
